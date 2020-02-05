d8b83987ab767863494017006dce2aecaaa0d1c0
eddsonblanco <eddsonrbr@gmail.com>
2020-02-04 21:39:59 -0800

    added cases loading and error

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,11 +1,23 @@
 import axios from 'axios';
-
+import { GET_ALL, LOADING, ERROR } from '../types/usersTypes'
 
 
 export const getAll = () => async ( dispatch ) =>{
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
     dispatch({
-        type: 'get_user',
-        payload: response.data
+        type: LOADING
     })
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        dispatch({
+            type: GET_ALL,
+            payload: response.data
+        })
+    } catch (error) {
+        console.log('Error: ', error.message)
+        dispatch({
+            type: ERROR,
+            payload: error.message
+        })
+        
+    }
 }
--- src/reducers/usersReducers.js
+++ src/reducers/usersReducers.js
@@ -1,12 +1,26 @@
+import { GET_ALL, LOADING, ERROR } from '../types/usersTypes'
+
+
 const INITIAL_STATE ={
-    users:  []
+    users:  [],
+    loading: false,
+    error: ''
 };
 
 
 export default (state = INITIAL_STATE, action) => {
     switch ( action.type)  {
-        case 'get_user':
-            return{ ...state, users : action.payload }
+        case GET_ALL:
+            return{ ...state,
+                users : action.payload,
+                loading: false
+             };
+
+        case LOADING:
+            return{ ...state, loading: true }
+
+        case ERROR:
+            return{ ...state, error: true, loading: false }
     
         default:
             return state;
